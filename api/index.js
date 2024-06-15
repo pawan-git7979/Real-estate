42ed65a008950fd90c4fa3f3cee86dbe20f09df4
pawan-git7979 <pawanagrawalp8472gmail.com>
2024-06-15 15:13:27 +0530

    Connect to database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,14 @@
-import { log } from 'console';
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config();
 
 const app = express();
-
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('Connected to database');
+}).catch((err)=>{
+    console.log(err);
+})
 
 
 app.listen(3000,()=>{
