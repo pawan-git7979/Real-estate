79785cc823b4a07346094e352e8e35476426cedd
pawan-git7979 <pawanagrawalp8472gmail.com>
2024-06-23 00:02:28 +0530

    complete listing page

--- client/src/pages/UpdateListing.jsx
+++ client/src/pages/UpdateListing.jsx
@@ -299,7 +299,7 @@ export default function CreateListing() {
               />
               <div className='flex flex-col items-center'>
                 <p>Regular price</p>
-                <span className='text-xs'>($ / month)</span>
+                <span className='text-xs'>(Rs / month)</span>
               </div>
             </div>
             {formData.offer && (
@@ -316,7 +316,7 @@ export default function CreateListing() {
                 />
                 <div className='flex flex-col items-center'>
                   <p>Discounted price</p>
-                  <span className='text-xs'>($ / month)</span>
+                  <span className='text-xs'>(Rs / month)</span>
                 </div>
               </div>
             )}
