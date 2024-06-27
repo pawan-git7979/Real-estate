83804f7adecda4a5a828187eec75e27585d945db
pawan-git7979 <pawanagrawalp8472gmail.com>
2024-06-27 15:37:00 +0530

    added pre-installation

--- api/index.js
+++ api/index.js
@@ -6,17 +6,16 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js'
 import cookieParser from 'cookie-parser';
 import listingRouter from './routes/listing.route.js';
+import path from 'path';
 
 
 dotenv.config();
 
 const app = express();
 app.use(express.json())
-app.use(cookieParser());
-// app.use(cors({
-//     origin: ' http://localhost:5173/*', // Replace with your client URL
-//     credentials: true // Allow credentials (cookies)
-//   }));
+app.use(cookieParser())
+
+const __dirname = path.resolve();
 
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log('Connected to database');
@@ -32,9 +31,11 @@ app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
 
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
-
-
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
